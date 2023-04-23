dd3e4b7c41645765da27922998712a5332bcd54e
Knieciu <gabennewell00@gmail.com>
2023-04-23 13:28:54 +0200

    creating function random number and testing

--- ColorFlipper/index.js
+++ ColorFlipper/index.js
@@ -1,8 +1,18 @@
-const = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const colors = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 let button = document.getElementById("switcher_button");
 let b_color = document.getElementById("background_color");
 
+function randomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
 button.addEventListener("click", function(){
-    console.log("test")
+    //console.log("test");
+
+    let end_color = "#";
+
+    for(let i = 0; i < 6; i++){
+        console.log(randomNumber());
+    }
+
 });
