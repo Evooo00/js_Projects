7d570d8aff0073022693fc3fc61b49c5c263a224
Knieciu <gabennewell00@gmail.com>
2023-04-23 13:17:05 +0200

    adding listener and testing

---
+++ ColorFlipper/index.js
@@ -0,0 +1,8 @@
+const = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+let button = document.getElementById("switcher_button");
+let b_color = document.getElementById("background_color");
+
+button.addEventListener("click", function(){
+    console.log("test")
+});
