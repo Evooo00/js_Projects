5e26343b188f721a29c8b401e7cb7b70de8fdc8e
Knieciu <gabennewell00@gmail.com>
2023-05-03 15:18:42 +0200

    Changed Hex tab code

--- ColorFlipper/hex.js
+++ ColorFlipper/hex.js
@@ -1,10 +1,22 @@
 
-const colors = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+function changingBackgroundColor(){
+    function randomNumber(){
+        return Math.floor(Math.random() * arrayOfHexChars.length);
+    }
 
+    const arrayOfHexChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+    const button = document.querySelector(".btn");
+    const textWithHexOfBackgroundColor = document.querySelector("#backgroundColor");
 
+    const randomHexChars = Array.from({length: 6}, () => arrayOfHexChars[randomNumber()]);   
+    const addingToHashCharColor = "#" + randomHexChars.join("");
+    
+    //console.log(addingToHashCharColor);
 
-function randomNumber(){
-    return Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = addingToHashCharColor;
+    button.style.backgroundColor = addingToHashCharColor;
+    textWithHexOfBackgroundColor.textContent = addingToHashCharColor;
+
 }
 
 
