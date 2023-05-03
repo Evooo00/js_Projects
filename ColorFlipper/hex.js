8a23a607fd80119b0f3a34ad61b61ce4840a084e
Knieciu <gabennewell00@gmail.com>
2023-05-03 13:56:12 +0200

    Changed Simple Tab Code

--- ColorFlipper/hex.js
+++ ColorFlipper/hex.js
@@ -1,35 +1,10 @@
 
-const colors = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const colors = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
 
-let button = document.getElementById("switcher_button");
-let b_color = document.getElementById("background_color");
+
 
 function randomNumber(){
     return Math.floor(Math.random() * colors.length);
 }
-button.addEventListener("click", function(){
-    //console.log("test");
 
-    let end_color = "#";
 
-    for(let i = 0; i < 6; i++){
-        //console.log(randomNumber());
-
-        end_color += colors[randomNumber()];
-        //console.log(end_color);
-    }
-
-    b_color.textContent = end_color;
-    document.body.style.backgroundColor = end_color;
-    button.style.backgroundColor = end_color;
-
-});
-
-button.addEventListener('mouseover', function(){
-
-    button.style.backgroundColor = "black";
-});
-button.addEventListener('mouseout', function(){
-
-    button.style.backgroundColor = "initial";
-});
--- ColorFlipper/index.js
+++ ColorFlipper/index.js
@@ -1,30 +1,20 @@
 
-const colors = ["red", "blue", "purple", "green"];
-
-let button = document.getElementById("switcher_button");
-let b_color = document.getElementById("background_color");
-
-function randomNumber(){
-    return Math.floor(Math.random() * colors.length);
-}
-button.addEventListener("click", function(){
-    //console.log("test");
-
-    let end_color = colors[randomNumber()];
-
+function changingBackgroundColor(){
     
+    const arrayWithColors = ['red', 'blue', 'green', 'orange'];
+    const textWithBackgroundColor = document.querySelector("#backgroundColor");
+    const button = document.querySelector(".btn");
 
-    b_color.textContent = end_color;
-    document.body.style.backgroundColor = end_color;
-    button.style.backgroundColor = end_color;
+    function randomNumberOfArray(){
+        return Math.floor(Math.random() * arrayWithColors.length);
+    }
 
-});
+    const generatedRandomColor = arrayWithColors[randomNumberOfArray()];
 
-button.addEventListener('mouseover', function(){
+    textWithBackgroundColor.textContent = generatedRandomColor;
+    document.body.style.backgroundColor = generatedRandomColor;
+    button.style.backgroundColor = generatedRandomColor;
+}
 
-    button.style.backgroundColor = "black";
-});
-button.addEventListener('mouseout', function(){
 
-    button.style.backgroundColor = "initial";
-});
+
