f5dbe25c0786a5399c8598eea86f674a116d872c
Knieciu <gabennewell00@gmail.com>
2023-06-15 22:19:40 +0200

    Added Review dict

---
+++ script.js
@@ -0,0 +1,26 @@
+const reviews =[
+    {
+        id: 1,
+        name: "Mariusz Pudzianowski",
+        job: "Strongman",
+        img: "pudzian.jpg",
+        text:
+        "Jak zawsze tanio skóry nie sprzedam ale musze przyznać, że strona jest naprawdę koksem prawie tak samo Jak moja forma na zawodach Strongman"
+    },
+    {
+        id: 2,
+        name: "Adam Małysz",
+        job: "Narciarz Farciarz",
+        img: "malysz.jpg",
+        text: 
+        "Wskoczyłem na tą strone jak na podium narciarskie moimi nartami i musze smiało stwierdzić, że doznania z użytkowania jej są lepsze niż skok ze skoczni :)"
+    },
+    {
+        id: 3,
+        name: "Robert Makłowicz",
+        job: "Kucharz Filantrop",
+        img: "maklowicz.jpg",
+        text:
+        "Ulala kiedyś byłem w Nigerii i tam jadłem wszystko co ludzie potrafili złapać w ręce i zapijałem pyszniuutką wódeczką tak tak, strona bardzo dobra"
+    }
+]
