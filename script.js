f260669ec16aa40ccadfc8341a1b551a311acac6
Knieciu <gabennewell00@gmail.com>
2023-06-16 18:45:03 +0200

    created all styles

---
+++ script.js
@@ -0,0 +1,6 @@
+const modalOpenButton = document.querySelector(".modalOpenButton");
+const modalCloseButton = document.querySelector(".modalCloseButton");
+
+modalOpenButton.addEventListener("click", function(){
+alert("test");
+});
