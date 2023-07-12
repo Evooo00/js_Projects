d7cdd87adc1cf3825d71648a44e487351f773e8f
Knieciu <gabennewell00@gmail.com>
2023-07-12 12:46:26 +0200

    idk

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ const modalOpenButton = document.querySelector(".modalOpenButton");
 const modalCloseButton = document.querySelector(".modalCloseButton");
 const modalView = document.querySelector(".modalView");
 
-modalOpenButton.addEventListener("click", function(){
-modalView.classList.add("showModalContent");
+
+modalOpenButton.addEventListener("click", function showModal(){
+    modalView.classList.add("showModalContent");
+
+    modalOpenButton.removeEventListener("click", showModal);
+
+    
 });
 
-modalCloseButton.addEventListener("click", function(){
+
+modalCloseButton.addEventListener("click", function hideModal(){
     modalView.classList.remove("showModalContent");
+
+    modalCloseButton.removeEventListener("click", hideModal);
     });
