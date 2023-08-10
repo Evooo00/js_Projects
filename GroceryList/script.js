99c068a964774532bf990397d6b03d9b73053257
Knieciu <gabennewell00@gmail.com>
2023-08-10 16:04:22 +0200

    Adding items works

--- GroceryList/script.js
+++ GroceryList/script.js
@@ -11,10 +11,14 @@ mainButton.addEventListener("click", function(){
 
     const attr = document.createAttribute("data");
     attr.value = id;
-    
-    console.log(attr);
+    element.setAttributeNode(attr);
+    element.innerHTML = `
+    <p class='itemLine'>${groceryItem.value}</p>
+    <button class='edit'>Edit</button>
+    <button class='remove'>Remove</button>`
+    console.log(element);
 
-    
+    items.appendChild(element);
     
             /*
             <div class='divForItem'>
