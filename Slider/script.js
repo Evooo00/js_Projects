9479e35adbef3784fe0b38a6b49b83a78fc17d99
Knieciu <gabennewell00@gmail.com>
2023-09-01 12:06:34 +0200

    End but with other animation

--- Slider/script.js
+++ Slider/script.js
@@ -12,9 +12,38 @@ let number = 1;
 buttons.forEach(function(btns){
     btns.addEventListener("click", function(e){
         const a = e.currentTarget.classList;
-        if(a.contains("next")){
-            console.log("asd")
-        };
+        if(a.contains("next") && number < 3){
+            //console.log("asd")
+            number += 1;
+            console.log(number)
+            
+        }
+        if(a.contains("previous") && number > 1){
+            //console.log("asd")
+            number -= 1;
+            console.log(number)
+            
+        }
+        
+
+        if(number == 1){
+            slide1.style.opacity = "1";
+            slide2.style.opacity = "0";
+            slide3.style.opacity = "0";
+            console.log(number)
+        }
+        else if(number == 2){
+            slide1.style.opacity = "0";
+            slide2.style.opacity = "1";
+            slide3.style.opacity = "0";
+            console.log(number)
+        }
+        else if(number == 3){
+            slide1.style.opacity = "0";
+            slide2.style.opacity = "0";
+            slide3.style.opacity = "1";
+            console.log(number)
+        }
 
         
     });
